391a6010235d94b89a57bf76ebc0c9cda942cf36
tiaot33 <lyq12123@gmail.com>
2021-11-23 17:57:54 +0800

    build: dockerfile

---
+++ docker/build.sh
@@ -0,0 +1,1 @@
+docker build -f ./docker/Dockerfile -t dandan-scraper --build-arg  JAR_FILE=dandan-scraper.jar  .
--- src/main/java/com/tiaot33/DandanScraperApplication.java
+++ src/main/java/com/tiaot33/DandanScraperApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -18,13 +19,15 @@ public class DandanScraperApplication {
     ScraperAndMove scraperAndMove;
 
     public static void main(String[] args) {
-        SpringApplication.run(DandanScraperApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(DandanScraperApplication.class, args);
+        PathProperty bean = run.getBean(PathProperty.class);
+        System.out.println(bean.getTarget());
+        System.out.println(bean.getSource());
     }
 
     @SneakyThrows
-    @Scheduled(fixedDelay = 1000*60*60*12)
+    //@Scheduled(fixedDelay = 1000*60*60*12)
     public void sche(){
-        Thread.sleep(10000);
         scraperAndMove.scraperDirsAndMove();
     }
 
